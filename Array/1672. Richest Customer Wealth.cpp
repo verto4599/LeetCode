7f938dd0e2776d4d4f1529eb386756f0e9cbdfb6
Yash Shinde <49606615+verto4599@users.noreply.github.com>
2021-04-08 16:07:06 +0530

    Create 1672. Richest Customer Wealth.cpp

---
+++ Array/1672. Richest Customer Wealth.cpp
@@ -0,0 +1,12 @@
+int maximumWealth(vector<vector<int>>& accounts) {
+        
+        int max = 0,temp = 0;
+        for(int i = 0;i < accounts.size();i++){
+            temp = 0;
+            for(int j = 0;j < accounts[i].size();j++){
+                temp += accounts[i][j];
+            }
+            max = temp > max ? temp : max;
+        }
+        return max;
+    }
