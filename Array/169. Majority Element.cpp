6d0156e27e3dbed5d6d9dd3a49d8a616e7493f5a
Yash Shinde <49606615+verto4599@users.noreply.github.com>
2021-04-11 12:17:56 +0530

    Create 169. Majority Element.cpp

---
+++ Array/169. Majority Element.cpp
@@ -0,0 +1,10 @@
+int majorityElement(vector<int>& nums) {
+        int counter = 0, majority;
+        for (int num : nums) {
+            if (!counter) {
+                majority = num;
+            }
+            counter += num == majority ? 1 : -1;
+        }
+        return majority;
+    }
